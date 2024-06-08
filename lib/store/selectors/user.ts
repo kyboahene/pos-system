e466706e4c55d909ec829be0c0d993a809b2a73f
kyboahene <kyeiyaw437@gmail>
2024-06-08 15:58:24 +0000

    feat: added a selector to get the authentication status of the user
    Signed-off-by: kyboahene <kyeiyaw437@gmail>

--- lib/store/selectors/user.ts
+++ lib/store/selectors/user.ts
@@ -1,3 +1,4 @@
 import { RootState } from ".."
 
 export const getUser = (state: RootState) => state.user
+export const getUserAuthenticationStatus = (state: RootState) => state.user.isAuthenticated
