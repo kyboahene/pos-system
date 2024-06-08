f4e206127b096924cdb461057af6ebe3faa2328b
kyboahene <kyeiyaw437@gmail>
2024-06-08 15:59:34 +0000

    feat: added the dashboard page template
    Signed-off-by: kyboahene <kyeiyaw437@gmail>

---
+++ modules/layout/templates/dashboard-layout-template/index.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Navbar from "../../components/navbar";
+import Sidebar from "../../components/sidebar";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const DashboardLayoutTemplate = ({ children }: Props) => {
+  return (
+    <main className="relative">
+      <Navbar />
+      <div className="flex">
+        <Sidebar />
+        <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14">
+          <div className="w-full">
+            <div className="size-full flex flex-col gap-10">{children}</div>
+          </div>
+        </section>
+      </div>
+    </main>
+  );
+};
+
+export default DashboardLayoutTemplate;
