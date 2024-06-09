806dc9ce15d4220813406856cdc8a5ceb9485cb8
kyboahene <kyeiyaw437@gmail>
2024-06-09 06:30:07 +0000

    refactor: increase the padding at the top of the main content dashboard component
    Signed-off-by: kyboahene <kyeiyaw437@gmail>

--- modules/layout/templates/dashboard-layout-template/index.tsx
+++ modules/layout/templates/dashboard-layout-template/index.tsx
@@ -12,7 +12,7 @@ const DashboardLayoutTemplate = ({ children }: Props) => {
       <Navbar />
       <div className="flex">
         <Sidebar />
-        <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14">
+        <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-40 max-md:pb-14">
           <div className="w-full">
             <div className="size-full flex flex-col gap-10">{children}</div>
           </div>
