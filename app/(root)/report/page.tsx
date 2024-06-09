6a1bcab34581a208da832ced87a117b4adf4064e
kyboahene <kyeiyaw437@gmail>
2024-06-09 06:34:39 +0000

    feat: imported the the report page templated created
    Signed-off-by: kyboahene <kyeiyaw437@gmail>

---
+++ app/(root)/report/page.tsx
@@ -0,0 +1,8 @@
+import ReportPageTemplate from "@/modules/reports/templates/report-page-template";
+import React from "react";
+
+const Report = () => {
+  return <ReportPageTemplate />;
+};
+
+export default Report;
