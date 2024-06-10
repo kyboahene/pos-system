513aab03e3718ff2bf8d5b878b1159d5c66e6338
kyboahene <kyeiyaw437@gmail>
2024-06-10 17:28:28 +0000

    refactor: separated the the form into another component
    Signed-off-by: kyboahene <kyeiyaw437@gmail>

--- modules/home/components/create-product-drawer/index.tsx
+++ modules/home/components/create-product-drawer/index.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Button } from "@/modules/shared/button";
 import {
   Sheet,
@@ -7,77 +9,52 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/modules/shared/sheet";
-import { Minus, Plus } from "lucide-react";
-import Image from "next/image";
+import { MoveLeft } from "lucide-react";
 import React from "react";
 
-const CreateProductDrawer = () => {
+import CreateProductForm from "../create-product-form";
+
+type CreateProductDrawerProps = {
+  isOpen: boolean;
+  setIsOpen: (value: boolean) => void;
+};
+
+const CreateProductDrawer = ({
+  setIsOpen,
+  isOpen,
+}: CreateProductDrawerProps) => {
   return (
-    <Sheet>
+    <Sheet open={isOpen}>
       <SheetTrigger className="cursor-pointer" asChild>
-        <Button variant="outline" className="py-4 px-6">
+        <Button
+          variant="outline"
+          className="py-4 px-6"
+          onClick={() => setIsOpen(true)}
+        >
           Add Product
         </Button>
       </SheetTrigger>
 
       <SheetContent
         side="right"
+        onInteractOutside={(e) => e.preventDefault()}
         className="border-none bg-white md:min-w-[755px] border-2 border-red-500"
       >
         <SheetHeader className="border-b pb-2">
-          <SheetTitle className="font-bold text-2xl">Add to Cart</SheetTitle>
+          <SheetTitle className="font-bold flex justify-between pl-4 pr-2">
+            <h1 className="text-2xl">Add Product</h1>
+            <button
+              className="flex items-center gap-3 z-50 bg-white"
+              onClick={() => setIsOpen(false)}
+            >
+              <MoveLeft />
+              <span className="font-bold">Back</span>
+            </button>
+          </SheetTitle>
         </SheetHeader>
         <div className="flex h-[calc(100vh-72px)] flex-col justify-between overflow-y-auto">
           <SheetClose asChild>
-            <form className="h-full flex flex-col justify-between">
-              <div className="flex h-full flex-col">
-                <div className="flex gap-6 py-6 border-b">
-                  <div className="relative h-[129px] w-[203px] rounded-lg overflow-hidden">
-                    <Image
-                      alt="product"
-                      blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mPsXLK8HgAF5wJVnJv1MQAAAABJRU5ErkJggg=="
-                      src="/images/pizza.jpg"
-                      objectFit="cover"
-                      fill
-                    />
-                  </div>
-                  <div className="flex flex-col gap-4 text-xl">
-                    <p className="">4 Meat Pizza</p>
-                    <p className="font-bold">GHS 50</p>
-                  </div>
-                </div>
-                <div className="flex flex-col gap-3 py-6">
-                  <h5 className="font-bold text-[22px]">Select</h5>
-
-                  <div className="flex flex-col gap-4 text-[18px]">
-                    <div className="flex items-center">
-                      <p className="flex-1">Small</p>
-                      <p className="flex-1 text-center">GHS 400</p>
-                      <div className="flex-1 flex justify-end items-center gap-3">
-                        <div className="rounded-2xl border bg-[#DFDFDF] py-1 px-2">
-                          <Plus className="w-4 h-4 cursor-pointer" />
-                        </div>
-                        <span className="rounded-lg px-2 border">0</span>
-                        <div className="rounded-2xl border bg-[#DFDFDF] py-1 px-2">
-                          <Minus className="w-4 h-4 cursor-pointer" />
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div className="py-6 border-t">
-                <div className="flex justify-between items-center">
-                  <div className="flex flex-col">
-                    <p>Total</p>
-                    <span className="font-bold text-3xl">0</span>
-                  </div>
-
-                  <Button>Add to Cart</Button>
-                </div>
-              </div>
-            </form>
+            <CreateProductForm />
           </SheetClose>
         </div>
       </SheetContent>
