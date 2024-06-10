88f5732d72c1b107c39e22925cb8760f3bab8ce2
kyboahene <kyeiyaw437@gmail>
2024-06-10 22:16:04 +0000

    feat: added utility functions for getting base64 of an image, name of products ordered and total orders made
    Signed-off-by: kyboahene <kyeiyaw437@gmail>

--- lib/utils/index.ts
+++ lib/utils/index.ts
@@ -1,3 +1,4 @@
+import { Cart } from "@/types";
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
@@ -6,10 +7,32 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 
-export const mockAPICall = async () => {
+export async function mockAPICall() {
     return new Promise((resolve) => setTimeout(resolve, 1000));
 };
 
-export const parseErrorMessage = async (errMessage: string, key: string) => {
-    return errMessage ? errMessage.includes(key) : ''
+export function parseErrorMessage(errMessage: string, key: string) {
+    if (!errMessage)
+        return ''
+
+    return errMessage.includes(key) ? errMessage : ''
+}
+
+export function getBase64(file: File): Promise<string> {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result as string);
+        reader.onerror = error => reject(error);
+    });
+}
+export function getNameOfProductsOrdered(products: Cart[]) {
+    return products.map((product) => `${product.product?.name}, `);
+}
+
+export function getTotalPricePaid(products: Cart[]) {
+    return products.reduce(
+        (sum, product) => sum + parseInt(product.total ?? ""),
+        0
+    );
 }
