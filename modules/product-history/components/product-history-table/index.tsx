e7426dfaa31eef178a7fd655069943ae82dd9377
kyboahene <kyeiyaw437@gmail>
2024-06-10 17:34:35 +0000

    feat: retrieved the actual data from the store and added a delete mutation
    Signed-off-by: kyboahene <kyeiyaw437@gmail>

--- modules/product-history/components/product-history-table/index.tsx
+++ modules/product-history/components/product-history-table/index.tsx
@@ -1,20 +1,73 @@
+import React from "react";
+import { toast } from "sonner";
+import { Trash2 } from "lucide-react";
+import { utils, writeFile } from "xlsx";
+import { useMutation } from "react-query";
+import { useDispatch, useSelector } from "react-redux";
+
+// shared
 import {
   Table,
+  TableRow,
   TableBody,
   TableCell,
-  TableFooter,
   TableHead,
   TableHeader,
-  TableRow,
 } from "@/modules/shared/table";
-import { Trash2 } from "lucide-react";
-import React from "react";
+
+// types
+import { Product, ProductVariant } from "@/types";
+
+// services
+import { removeProduct } from "@/lib/services/product";
+
+// store
+import { createProduct } from "@/lib/store/slices/product";
+import { getProducts } from "@/lib/store/selectors/product";
+
+// hooks
+import useLocalStorage from "@/lib/hooks/use-local-storage";
 
 const ProductHistoryTable = () => {
-  const data: any[] | undefined = [1, 2, 3, 4, 5];
+  const dispatch = useDispatch();
+  const products = useSelector(getProducts);
+  const { setItem } = useLocalStorage("products");
+
+  const { mutate, isLoading } = useMutation(
+    "delete-product",
+    (productId: string) => removeProduct(productId),
+    {
+      onSuccess: (data: Product[]) => {
+        setItem(data);
+        dispatch(createProduct(data));
+        toast.success("Product deleted successfully");
+      },
+    }
+  );
+
+  function getVariantNames(variants: ProductVariant[]) {
+    return variants.map((variant) => `${variant.name}, `);
+  }
+
+  function handleDelete(productId: string) {
+    mutate(productId);
+  }
+
+  const exportToExcel = (sheet1: unknown[], date: any) => {
+    let firstSheet = utils.json_to_sheet(sheet1, {
+      dateNF: "dd/mm/yy",
+    });
+
+    let leaveBook = utils.book_new();
+    utils.book_append_sheet(leaveBook, firstSheet, "Product History");
+    writeFile(leaveBook, `${date}.xls`, {
+      cellDates: true,
+    });
+  };
+
   return (
     <section className="bg-white">
-      <div className=" max-sm:max-w-[300px] overflow-x-auto">
+      <div className=" max-sm:max-w-[370px] overflow-x-auto">
         <Table className="min-w-full text-xl">
           <TableHeader className="bg-[#EFEFEF]">
             <TableRow className="!border-b-0 font-bold">
@@ -26,18 +79,26 @@ const ProductHistoryTable = () => {
             </TableRow>
           </TableHeader>
           <TableBody>
-            {data !== undefined && data.length > 0 ? (
-              data?.map((job) => {
+            {isLoading ? (
+              <TableRow>
+                <TableCell colSpan={7} className="h-24 text-center">
+                  Loading…
+                </TableCell>
+              </TableRow>
+            ) : products !== undefined && products.length > 0 ? (
+              products?.map((product) => {
                 return (
-                  <TableRow key={job.id}>
-                    <TableCell className="">Fried Rice</TableCell>
-                    <TableCell className="">Assorted, Plain</TableCell>
+                  <TableRow key={product.id}>
+                    <TableCell className="">{product.name}</TableCell>
+                    <TableCell className="">
+                      {getVariantNames(product.variants)}
+                    </TableCell>
                     <TableCell className="">20</TableCell>
                     <TableCell className="">300</TableCell>
                     <TableCell className="flex items-center gap-2">
                       <span
                         className="cursor-pointer"
-                        // onClick={() => handleDelete(job)}
+                        onClick={() => handleDelete(product.id)}
                       >
                         <Trash2 className="text-red-500" />
                       </span>
@@ -46,14 +107,13 @@ const ProductHistoryTable = () => {
                 );
               })
             ) : (
-              <>
-                <p className="text-center">
+              <TableRow>
+                <TableCell colSpan={6} className="text-center py-4">
                   You have not upload any product yet.
-                </p>
-              </>
+                </TableCell>
+              </TableRow>
             )}
           </TableBody>
-          <TableFooter></TableFooter>
         </Table>
       </div>
     </section>
