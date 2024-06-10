bff40e32ff891ff316643d4044b2a0288d852c86
kyboahene <kyeiyaw437@gmail>
2024-06-10 22:07:17 +0000

    feat: received actual product data
    Signed-off-by: kyboahene <kyeiyaw437@gmail>

--- modules/home/components/product-card/index.tsx
+++ modules/home/components/product-card/index.tsx
@@ -1,28 +1,58 @@
+import { addSelectedProduct } from "@/lib/store/slices/cart";
+import { cn } from "@/lib/utils";
 import { Product } from "@/types";
-import { Star } from "lucide-react";
+import { Check, Star } from "lucide-react";
 import Image from "next/image";
 import React from "react";
+import { useDispatch } from "react-redux";
 
 type ProductCardProps = {
-  product?: Product;
+  product: Product;
+  isSelected: boolean;
+  setSelectedProduct: (value: Product) => void;
 };
 
-const ProductCard = ({ product }: ProductCardProps) => {
+const ProductCard = ({
+  product,
+  setSelectedProduct,
+  isSelected,
+}: ProductCardProps) => {
+  const dispatch = useDispatch();
+
+  function handleSelect(product: Product) {
+    setSelectedProduct(product);
+    dispatch(addSelectedProduct(product));
+  }
+
   return (
-    <section className="flex flex-col gap-3">
+    <section
+      className="relative flex flex-col gap-3 group cursor-pointer"
+      onClick={() => handleSelect(product)}
+    >
+      <button
+        className={cn(
+          "hidden group-hover:block absolute bg-white p-4 rounded-lg top-2 left-2 z-30",
+          {
+            "p-2 block": isSelected,
+          }
+        )}
+      >
+        {isSelected ? <Check className="h-4 w-4" /> : ""}
+      </button>
       <div className="relative h-[217px] w-full rounded-lg overflow-hidden">
         <Image
           alt="product"
           blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mPsXLK8HgAF5wJVnJv1MQAAAABJRU5ErkJggg=="
-          src="/images/pizza.jpg"
+          src={product?.image ? product.image : ""}
           objectFit="cover"
           fill
+          sizes=""
         />
       </div>
 
       <div className="flex flex-col gap-1 px-3">
         <div className="flex justify-between items-center ">
-          <p className="text-xl">4 Meat Pizza</p>
+          <p className="text-xl">{product?.name}</p>
           <span className="flex items-center gap-2">
             <Star className="text-[#FFA800]" />
             <span className="text-xl">3.5</span>
