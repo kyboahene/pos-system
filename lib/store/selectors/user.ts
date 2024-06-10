8645244bee978df521fc66d81cb25627ed7e591a
kyboahene <kyeiyaw437@gmail>
2024-06-10 22:23:01 +0000

    refactor
    Signed-off-by: kyboahene <kyeiyaw437@gmail>

--- lib/store/selectors/user.ts
+++ lib/store/selectors/user.ts
@@ -1,4 +1,4 @@
 import { RootState } from ".."
 
-export const getUser = (state: RootState) => state.user
+export const getUser = (state: RootState) => state.user.user
 export const getUserAuthenticationStatus = (state: RootState) => state.user.isAuthenticated
