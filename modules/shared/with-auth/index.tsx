fe2d8cb66fe2413661d639c51f6a9a75feed062b
kyboahene <kyeiyaw437@gmail>
2024-06-11 02:48:59 +0000

    feat: added another argument to indicate whether an route is public or not
    Signed-off-by: kyboahene <kyeiyaw437@gmail>

--- modules/shared/with-auth/index.tsx
+++ modules/shared/with-auth/index.tsx
@@ -1,59 +1,64 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 "use client";
 
-import useLocalStorage from "@/lib/hooks/use-local-storage";
+import { useDispatch } from "react-redux";
+import { useRouter } from "next/navigation";
+import React, { ComponentType, useEffect, useState } from "react";
+
+// store
 import { addCart } from "@/lib/store/slices/cart";
+import { setUser } from "@/lib/store/slices/user";
 import { addOrder } from "@/lib/store/slices/order";
 import { createProduct } from "@/lib/store/slices/product";
-import { setUser } from "@/lib/store/slices/user";
-import { useRouter } from "next/navigation";
-import React, { ComponentType, useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { Skeleton } from "../skeleton";
+
+// hooks
+import useLocalStorage from "@/lib/hooks/use-local-storage";
 
 const withAuth = <P extends object>(
-  WrappedComponent: ComponentType<P>
+  WrappedComponent: ComponentType<P>,
+  isPublic: boolean = false
 ): React.FC<P> => {
   const WithAuthComponent: React.FC<P> = (props) => {
     const router = useRouter();
     const dispatch = useDispatch();
-    const { getItem } = useLocalStorage("user");
+    const { getItem: getUser } = useLocalStorage("user");
     const { getItem: getCart } = useLocalStorage("cart");
     const { getItem: getOrders } = useLocalStorage("orders");
     const { getItem: getProducts } = useLocalStorage("products");
 
-    const user = getItem();
-    const cart = getCart();
-    const orders = getOrders();
-    const products = getProducts();
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-      if (user) {
+      const user = getUser();
+      const cart = getCart();
+      const orders = getOrders();
+      const products = getProducts();
+
+      if (user?.isUserAuthenticated) {
         dispatch(setUser(user));
-        dispatch(addOrder(orders));
         dispatch(addCart(cart));
+        dispatch(addOrder(orders));
         dispatch(createProduct(products));
-      }
 
-      if (user === undefined && !user?.isUserAuthenticated) {
+        if (isPublic) {
+          router.push("/");
+        } else {
+          setLoading(false);
+        }
+      } else if (!isPublic) {
         router.push("/login");
+      } else {
+        setLoading(false);
       }
-    }, [router, user]);
+    }, [router, dispatch]);
 
-    if (!user)
+    if (loading) {
       return (
-        <div className="flex flex-col gap-4 mt-12">
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-            {[1, 2, 3, 4].map((i, index) => (
-              <Skeleton key={index} className="h-44 w-full rounded-lg" />
-            ))}
-          </div>
-          <div className="flex flex-col gap-4 lg:flex-row">
-            <Skeleton className="h-[300px] w-full rounded-lg" />
-          </div>
+        <div className="h-screen flex justify-center items-center">
+          <span className="spinner"></span>
         </div>
       );
-
+    }
     return <WrappedComponent {...props} />;
   };
 
