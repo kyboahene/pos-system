6abde9d30e57ddcbc0a907d81f9aa70f2134aa9d
kyboahene <kyeiyaw437@gmail>
2024-06-11 02:49:44 +0000

    made second parameter true indicating it as a public route
    Signed-off-by: kyboahene <kyeiyaw437@gmail>

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,8 +1,13 @@
+"use client";
+
 import React from "react";
+
+// components
+import withAuth from "@/modules/shared/with-auth";
 import LoginTemplate from "@/modules/auth/templates/login-template";
 
 const Login = () => {
   return <LoginTemplate />;
 };
 
-export default Login;
+export default withAuth(Login, true);
